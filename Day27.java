da6502db817b5c66b7676c6fc951e477982f7504
Sorapalli Venkatesh <sorapalli.venkatesh@gmail.com>
2017-04-17 13:20:02 +0530

    Testing

---
+++ Day27.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package pkg30daysofcode;
+
+/**
+ * @author S Y S Venkatesh
+ *
+ */
+public class Day27 
+{
+	public static void main(String[] args)
+	{
+		 System.out.println(5);
+	        System.out.println("2 2");
+	        System.out.println("1 2");
+	        
+	        System.out.println("3 2");
+	        System.out.println("0 -1 2");
+	        
+	        System.out.println("4 1");
+	        System.out.println("1 2 3 4");
+	        
+	        System.out.println("5 2");
+	        System.out.println("0 -2 -3 5 6");
+	        
+	        System.out.println("6 3");
+	        System.out.println("0 -2 3 5 6 7");
+	}
+}
